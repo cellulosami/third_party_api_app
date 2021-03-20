d675041478968df34f85d1fb08857df28f3bc4fe
austin shields <ajshields196@gmail.com>
2021-03-20 19:47:53 -0400

    add holidays api index route a hide credentials

--- app/controllers/api/holidays_controller.rb
+++ app/controllers/api/holidays_controller.rb
@@ -1,7 +1,7 @@
 class Api::HolidaysController < ApplicationController
   def index
-    require "http"
-    reponse = HTTP.get"https://holidayapi.com/v1/holidays?pretty&key=c63a52af-95be-443d-9378-a34aafd3b389&country=US&year=2020"
+    response = HTTP.get"https://holidayapi.com/v1/holidays?pretty&key=#{Rails.application.credentials.holidays_api[:api_key]}&country=US&year=2020"
+    p response.parse
     render json: response.parse
   end
 end
